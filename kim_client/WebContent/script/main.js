889768e3d0e41fd039dd0bb5186e15956de597fd
Kim-seongjun <01090524238@naver.com>
2020-02-17 13:21:29 +0900

    20.02.17 점심커밋

---
+++ kim_client/WebContent/script/main.js
@@ -0,0 +1,8 @@
+//js 공용 출력 메소드
+
+$(function() {
+	$('#header').load("/kim_client/include/header.html");
+	$('#nav').load("/kim_client/include/nav.html");
+	$('#aside').load("/kim_client/include/aside.html");
+	$('#footer').load("/kim_client/include/footer.html");
+});
