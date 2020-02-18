f96708417ab13a3ab39517c3aff88809f7084cd4
Kim-seongjun <01090524238@naver.com>
2020-02-18 15:55:30 +0900

    20.02.18 ui회원가입

--- kim_client/WebContent/script/main.js
+++ kim_client/WebContent/script/main.js
@@ -1,11 +1,7 @@
-//js 공용 출력 메소드
+const apiKey="6Bq2SNPavRoT8QHnv4Vj11p4IBuuzh";
+const username="bbbbbbbb12"; 
 
-$(function() {
-	$('#header').load("/kim_client/include/header.html");
-	$('#nav').load("/kim_client/include/nav.html");
-	$('#aside').load("/kim_client/include/aside.html");
-	$('#footer').load("/kim_client/include/footer.html");
-});
+
 //1.입력 체크 실패 메시지 출력
 function printErrorMessage(element, message) {
 	element.text(message).css({"color":"red","font-size":"0.75em"});
@@ -15,3 +11,29 @@ function printErrorMessage(element, message) {
 function printMessage(element, message) {
 	element.text(message).css({"color":"green","font-size":"0.75em"})
 }
+//서버에서 실패 응답이 왔을때
+function printError(xhr, massage) {
+	toastr.error(massage, "처리 오류");
+	console.log("에러코드:"+xhr.status+",에러 메시지:"+xhr.responseText);
+}
+
+//3. ES6 of 반복문 이용한 formData출력
+function fd(formData) {
+	for(var key of formData.keys())
+		console.log(key);
+	
+	for(var value of formData.values())
+		console.log(value);
+	
+}
+
+//js 공용 출력 메소드
+
+$(function() {
+	$('#header').load("/kim_client/include/header.html");
+	$('#nav').load("/kim_client/include/nav.html");
+	$('#aside').load("/kim_client/include/aside.html");
+	$('#footer').load("/kim_client/include/footer.html");
+});
+
+
